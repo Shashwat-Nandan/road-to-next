106d8e1c41ff0775e1235b2fba68dc2284a7192c
Shashwat Nandan <shashwat.nandan@propelytics.in>
2024-12-03 07:02:50 +0530

    add styling changes and shadcn

--- src/data.ts
+++ src/data.ts
@@ -3,14 +3,20 @@ const initialTickets = [
         id: 1,
         title: "Ticket 1",
         content: "Ticket 1 content",
-        status: "DONE",
+        status: "DONE" as const,
     },
     {
         id: 2,
         title: "Ticket 2",
         content: "Ticket 2 content",
-        status: "IN_PROGRESS",
-        },
+        status: "IN_PROGRESS" as const,
+    },
+    {
+        id: 3,
+        title: "Ticket 3",
+        content: "Ticket 3 content",
+        status: "NOT_STARTED" as const,
+    },
 ]
 
 export { initialTickets };
